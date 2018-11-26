8e0e40edb71481ff0f0b384cbc80227c555cb5f8
moshe sandmann <info@seeiceland.com>
2018-11-26 12:02:24 +0200

    finished

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+  layout 'profile_layout'
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    roof = Event.find(params[:roof_id])
+    @booking.event = event
+    @booking.user = current_user
+    # raise
+    if @booking.save
+      redirect_to bookings_path, notice: 'Your booking was successfully created.'
+    else
+      render 'events/show', notice: 'Something went wrong. Could not book roof!'
+    end
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :period, :user_id)
+  end
 end
+
+
