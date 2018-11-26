01650d410be87e81068a7b6edc31449f80471292
alan glam <alanhaimglam@gmail.com>
2018-11-26 11:13:15 +0200

    models & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_142108) do
+ActiveRecord::Schema.define(version: 2018_11_26_085758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "sport_id"
+    t.datetime "date"
+    t.integer "duration"
+    t.string "address"
+    t.integer "capacity"
+    t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sport_id"], name: "index_events_on_sport_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +57,8 @@ ActiveRecord::Schema.define(version: 2018_11_25_142108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "events", "sports"
+  add_foreign_key "events", "users"
 end
