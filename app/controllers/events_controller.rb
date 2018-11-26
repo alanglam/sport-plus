367a8d420f51be9330621bd6dcacc3523a3170b6
moshe sandmann <info@seeiceland.com>
2018-11-26 17:44:44 +0200

    created events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,15 @@
 class EventsController < ApplicationController
  def index
     @events = Event.all
+    @sport = Sport.find(params[:sport_id])
   end
 
   def show
+    @sport = Sport.find(params[:id])
     @event = Event.find(params[:id])
+    @booking = Booking.find(params[:id])
+
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'dashboard', to: 'users#profile'
 resources :sports, only: [:show, :index] do
   resources :events, only: [:show, :index] do
     resources :bookings
   end
 end
  root "pages#home"
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
