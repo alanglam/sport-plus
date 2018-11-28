5129349f4afd46767b54c3ca6180d6363a3ffb10
alan glam <alanhaimglam@gmail.com>
2018-11-28 16:09:14 +0200

    geocode

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,22 @@
 class EventsController < ApplicationController
+  layout 'map', only: :index
   def index
+    @events = Event.where.not(latitude: nil, longitude: nil)
+
+
     city_query = params[:search][:city_query]
     sport_query = params[:search][:sport_query]
     if city_query.present? && sport_query.present?
       @sport = Sport.find(sport_query)
       @events = Event.where(city: city_query, sport_id: @sport.id)
+
+       @markers = @events.map do |event|
+      {
+        lng: event.longitude,
+        lat: event.latitude,
+        infoWindow: { content: render_to_string(partial: "/events/map_window", locals: { event: event }) }
+      }
+    end
     else
       redirect_to sports_path
     end
