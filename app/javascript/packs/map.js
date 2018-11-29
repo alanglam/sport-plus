3d921fe077f6f85d4e25814ce1b8c28c59afb6ff
alan glam <alanhaimglam@gmail.com>
2018-11-29 16:18:35 +0200

    autocomplete

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,6 +3,17 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
+
+
+const addressInput = document.getElementById('event_address');
+
+if (addressInput) {
+  const places = require('places.js');
+  const placesAutocomplete = places({
+    container: addressInput
+  });
+}
+
 const mapElement = document.getElementById('map');
 
 if (mapElement) {
