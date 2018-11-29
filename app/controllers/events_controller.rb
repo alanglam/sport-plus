3c4da0c9e4108784311f4a5adc730fe8e81f26d0
Leah Sadoun <leahsadoun@hotmail.fr>
2018-11-29 16:18:39 +0200

    branch 25 solved

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,20 +2,18 @@ class EventsController < ApplicationController
   def index
     @events = Event.where.not(latitude: nil, longitude: nil)
 
-
     city_query = params[:search][:city_query]
     sport_query = params[:search][:sport_query]
     if city_query.present? && sport_query.present?
       @sport = Sport.find(sport_query)
       @events = Event.where(city: city_query, sport_id: @sport.id)
-
-       @markers = @events.map do |event|
+      @markers = @events.map do |event|
       {
         lng: event.longitude,
         lat: event.latitude,
         infoWindow: { content: render_to_string(partial: "/events/map_window", locals: { event: event }) }
       }
-    end
+      end
     else
       redirect_to sports_path
     end
@@ -27,11 +25,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @sport = Sport.find(params[:sport_id])
-    @event.sport = sport
+    @sport = Sport.find(params['event']['sport'])
+    @event.sport = @sport
     @event.user = current_user
     if @event.save
-      redirect_to events_path, notice: 'Your event was successfully created.'
+      redirect_to root_path, notice: 'Your event was successfully created.' #redirect to activity path
     else
       render 'events/new', notice: 'Something went wrong. Could not create your event!'
     end
@@ -46,6 +44,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:date, :duration, :address, :user_id, :sport_id)
+    params.require(:event).permit(:capacity, :latitude, :longitude, :date, :duration, :address, :user_id, :sport_id, :city)
   end
 end
