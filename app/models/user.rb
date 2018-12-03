7436ecc93a64940428f1041793f49a7a1d487c4c
Leah Sadoun <leahsadoun@hotmail.fr>
2018-12-03 18:20:11 +0200

    activity

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook]
 has_many :messages, dependent: :destroy
+has_many :bookings
 mount_uploader :photo, PhotoUploader
 
 def self.from_omniauth(auth)
