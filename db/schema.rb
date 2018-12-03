718c19e4bf924ce731506c80ee8195548e3f75d6
alan glam <alanhaimglam@gmail.com>
2018-12-03 15:03:18 +0200

    bugfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_12_03_113623) do
-
-
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,8 +75,6 @@ ActiveRecord::Schema.define(version: 2018_12_03_113623) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "provider"
-    t.string "uid"
     t.string "photo"
     t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
